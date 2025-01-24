33191482dec244cd0d920aa36e85d021f737f8aa
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Air.local>
2025-01-24 21:35:10 +0530

    removed dist folder and added layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,8 @@
 import './App.css';
-import { Routes,Route } from 'react-router';
-
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<h1>hello from abhishek</h1>}/>
-    </Routes>
+    <h1>Hi This is abhishek</h1>
   )
 }
 
--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -1,10 +1,12 @@
 import {
-    type RouteConfig,
-    route,
-  } from "@react-router/dev/routes";
-  
-  export default [
+  type RouteConfig,
+  route,
+  layout
+} from "@react-router/dev/routes";
+
+export default [
+  layout("./layout/layout.tsx", [
     route("/", "./App.tsx"),
-    route("*?", "./pages/404/notFound.tsx"),
-  ] satisfies RouteConfig;
-  
+    route("*", "./pages/404/notFound.tsx"),
+  ])
+] satisfies RouteConfig;
